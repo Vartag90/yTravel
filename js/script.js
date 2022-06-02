7c52794a9c1175e645c71e0e7da48a3f012d290a
Vartag90 <maks.enikarp.90@gmail.com>
2022-06-02 16:11:56 +0300

    new

--- js/script.js
+++ js/script.js
@@ -6,9 +6,8 @@
 	let parents = document.querySelectorAll(root);
 
 	for (const elem of parents) {
-		let img = elem.firstElementChild;
-		let imgSrc = img.src.startsWith('http') ? img.src.slice(-18) : img.src;
-		img.classList.add('hidden');
+		let imgSrc = elem.firstElementChild.getAttribute('src');
+		elem.firstElementChild.classList.add('hidden');
 
 		elem.style.backgroundImage = `url('${imgSrc}')`;
 		elem.classList.add('imageFit');
