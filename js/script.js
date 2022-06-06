ab6868610e462c4bcbfcc0fb9fa29a35f8672d38
Vartag90 <maks.enikarp.90@gmail.com>
2022-06-06 23:54:58 +0300

    last

--- js/script.js
+++ js/script.js
@@ -1,17 +1,58 @@
 'use strict';
 
+let parentsImage = document.querySelectorAll('.ibg');
 
-(function (root) {
+for (const elem of parentsImage) {
+    let imgSrc = elem.firstElementChild.getAttribute('src');
+    elem.firstElementChild.classList.add('hidden');
 
-	let parents = document.querySelectorAll(root);
+    elem.style.backgroundImage = `url('${imgSrc}')`;
+    elem.classList.add('imageFit');
+}
 
-	for (const elem of parents) {
-		let imgSrc = elem.firstElementChild.getAttribute('src');
-		elem.firstElementChild.classList.add('hidden');
 
-		elem.style.backgroundImage = `url('${imgSrc}')`;
-		elem.classList.add('imageFit');
-	}
 
-})('.ibg')
+const parentSlider = document.querySelector('.slider');
+let position = 0;
+const slidesToShow = 1;
+const slidesToScroll = 1;
+const sliderContainer = parentSlider.querySelector('.slider__container');
+const sliderBody = parentSlider.querySelector('.slider__body');
+const sliderItems = parentSlider.querySelectorAll('.slider__item');
+const itemsCount = sliderItems.length;
+const btnPrev = parentSlider.querySelector('.btn-next');
+const btnNext = parentSlider.querySelector('.btn-prev');
+const itemWidth = sliderContainer.clientWidth / slidesToShow;
+const movePosition = itemWidth * slidesToScroll;
 
+sliderItems.forEach(function(elem) {
+    elem.style.minWidth = `${itemWidth}px`;
+})
+
+btnNext.addEventListener('click', function() {
+    const itemLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
+    position -= itemLeft >= slidesToScroll ? movePosition : itemLeft * itemWidth;
+    setPosition();
+    checkBtns();
+})
+
+btnPrev.addEventListener('click', function() {
+    const itemLeft = Math.abs(position) / itemWidth;
+
+    position += itemLeft >= slidesToScroll ? movePosition : itemLeft * itemWidth;
+
+    setPosition();
+    checkBtns();
+})
+
+
+const setPosition = () => sliderBody.style.transform = `translateX(${position}px)`;
+
+const checkBtns = () => {
+    btnPrev.disabled = position === 0;
+    btnNext.disabled = position <= -(itemsCount - slidesToShow) * itemWidth;
+}
+
+checkBtns();
+
+console.log(sliderContainer.clientWidth);
